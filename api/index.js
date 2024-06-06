0fd602b67bd4c7442cef5db112befac7312f2539
Bruno Daher Coelho Oliveira <brunouk@MacBook-Pro-de-Bruno.local>
2024-06-05 21:34:00 -0300

    1alt

--- api/index.js
+++ api/index.js
@@ -30,37 +30,7 @@ const html = `
     />
   </head>
   <body>
-    <h1>Vercel + Teste de API</h1>
-    <p>
-      This is a starter template for Vercel + Fastify. Requests are
-      rewritten from <code>/*</code> to <code>/api/*</code>, which runs
-      as a Vercel Function.
-    </p>
-    <p>
-        For example, here is the boilerplate code for this route:
-    </p>
-    <pre>
-<code>import Fastify from 'fastify'
-
-const app = Fastify({
-  logger: true,
-})
-
-app.get('/', async (req, res) => {
-  return res.status(200).type('text/html').send(html)
-})
-
-export default async function handler(req: any, res: any) {
-  await app.ready()
-  app.server.emit('request', req, res)
-}</code>
-    </pre>
-    <p>
-    <p>
-      <a href="https://vercel.com/templates/other/fastify-serverless-function">
-      Deploy your own
-      </a>
-      to get started.
+   BEM VINDO AO TESTE DE API
   </body>
 </html>
 `
